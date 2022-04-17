d7c3e0736e5424d2bc9ab1130e42be126c833b83
Wang <winnnie67672002@yahoo.com.tw>
2022-04-17 20:30:38 +0800

    commit

---
+++ Shop1/Controllers/MyshopController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop1.Models;
+
+namespace Shop1.Controllers
+{
+    public class MyshopController : Controller
+    {
+        private static List<Myshop> _myshop = new List<Myshop>();
+        // GET: MyshopController
+        public ActionResult Index()
+        {
+            return View(_myshop);
+        }
+
+        // GET: MyshopController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MyshopController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MyshopController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Myshop collection)
+        {
+            try
+            {
+                _myshop.Add(collection);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MyshopController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MyshopController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MyshopController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MyshopController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
